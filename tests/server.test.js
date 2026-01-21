8d17863bc6106231a9f2becc471add534750d9a2
Marven Mathelier <marven250@hotmail.com>
2026-01-21 11:58:14 -0500

    tenth push

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,27 +3,6 @@ const app = require('../app');
 
 describe('Node.js Server API', () => {
 
-  describe('GET /', () => {
-    it('should return server running message', async () => {
-      const res = await request(app).get('/');
-
-      expect(res.statusCode).toBe(200);
-      expect(res.text).toBe('Node.js server is running 🚀');
-    });
-  });
-
-  describe('GET /api/hello', () => {
-    it('should return hello message', async () => {
-      const res = await request(app).get('/api/hello');
-
-      expect(res.statusCode).toBe(200);
-      expect(res.body).toEqual({
-        message: 'Hello from the server!'
-      });
-    });
-  });
-
-  });
 
   describe('404 handler', () => {
     it('should return 404 for unknown routes', async () => {
